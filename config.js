30f25ebd40f6cce86cad0d2400cd0179e226bb18
Olive-TCS <jonathan.olive-tcs@hpe.com>
2023-10-01 20:51:37 -0600

    commit 3 ultimate completed

--- config.js
+++ config.js
@@ -5,41 +5,35 @@ const timeStamp = new Date();
 
 const dates = {
     sinceYear:2023,
-    sinceMount:8,
-    sinceDate:27,
-    sinceHour:0,
-    sinceMinute:0,
+    sinceMount:9,
+    sinceDate:21,
     untilYear:2023,
     untilMount:9,
-    untilDate:5,
-    untilHour:0,
-    untilMinute:0
+    untilDate:30,
 }
 
-const initialSinceDate =  moment(`${dates.sinceYear}-${(Number(dates.sinceMount)).toString().padStart(2,0)}-${(Number(dates.sinceDate)).toString().padStart(2,0)} ${(Number(dates.sinceHour)).toString().padStart(2,0)}:${(Number(dates.sinceMinute)).toString().padStart(2,0)}:00`).format( "YYYY-MM-DD HH:mm");
-const initialUntilDate = moment(`${dates.untilYear}-${(Number(dates.untilMount)).toString().padStart(2,0)}-${(Number(dates.untilDate)).toString().padStart(2,0)} ${(Number(dates.untilHour)).toString().padStart(2,0)}:${(Number(dates.untilMinute)).toString().padStart(2,0)}:00`).format( "YYYY-MM-DD HH:mm");
-
+const initialSinceDate = moment(`${dates.sinceYear}-${(Number(dates.sinceMount)).toString().padStart(2,0)}-${(Number(dates.sinceDate)).toString().padStart(2,0)} 06:00:00`).format( "YYYY-MM-DD HH:mm");
+const initialUntilDate = moment(`${dates.untilYear}-${(Number(dates.untilMount)).toString().padStart(2,0)}-${(Number(dates.untilDate)).toString().padStart(2,0)} 06:00:00`).format( "YYYY-MM-DD HH:mm");
 
 const apiKey = `SSWS 007jqNreyFLfm76qRcUaO2b3tfZYudeDs06DvVfL4F`
 
-
-let records = 0
-
 const getData = (discardHeaders = false,url) => {
-    console.log({ url });
+    //console.log({ url });
     return new Promise((resolve, reject) => {
         axios
             .get(url, { headers: { Authorization: apiKey } })
             .then((resp) => {
                 const lines = resp.data.split("\n");
-                //console.log('length',lines.length);
+                console.log('lines',lines)
                 if (discardHeaders) {
-                    console.log(lines.length -1,' records') 
+                    console.log('length without',lines.length -2 );
                     const linesWithoutHeaders = lines.slice(1).join("\n");
-                    resolve(linesWithoutHeaders);
+                    //console.log(linesWithoutHeaders);
+                    resolve({data:linesWithoutHeaders,records:(lines.length - 2)});
                 } else {
-                    console.log(lines.length,' records') 
-                    resolve(resp.data);
+                    console.log('length with headers',lines.length-3);
+                    //console.log(resp.data)
+                    resolve({data:resp.data,records:lines.length - 3});
                 }
             })
             .catch((error) => {
@@ -48,18 +42,13 @@ const getData = (discardHeaders = false,url) => {
     });
 };
 
-
-
-
 module.exports = {
-    url : "https://hpe-gl-dev2-hub.oktapreview.com/sage/api/v1/logs/csv?",
+    url : "https://hpe-gl-dev2-hub-admin.oktapreview.com/sage/api/v1/logs/csv?",
     path: `${timeStamp.getFullYear()}-${(timeStamp.getMonth() + 1).toString().padStart(2, 0)}-${timeStamp.getDate()}T${timeStamp.getHours()}-${timeStamp.getMinutes()}-logs.csv`,
     dates,
     initialSinceDate,
     initialUntilDate,
     getData,
-    records
-
 }
 
 const urlComplete = "https://hpe-gl-dev2-hub.oktapreview.com/sage/api/v1/logs/csv?since=2023-09-22T06%3A00%3A00Z&until=2023-09-23T05%3A59%3A59Z&q="
--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-const axios = require("axios");
 const fs = require("fs");
 const moment = require("moment");
 const {
@@ -10,23 +9,57 @@ const {
 } = require("./config");
 
 
-const diffDays = moment(initialUntilDate).diff(initialSinceDate, "days");
-const firstcall = 0;
-console.log({diffDays});
-
-if(diffDays < 0){
-    throw new Error('since dates must be lower then until dates')
+if (!moment(initialSinceDate).isValid() || !moment(initialUntilDate).isValid()) {
+    throw new Error('invalid dates')
 }
 
+const diffDays = moment(initialUntilDate).diff(initialSinceDate, "days");
+const firstcall = 0;
+let interval = undefined
+let logs = 0;
+let i = 0;
+console.log({ diffDays });
 
+if (diffDays < 0) {
+    throw new Error('since dates must be lower then until dates')
+}
 
 let sinceDate = moment(initialSinceDate);
 let untilDate = moment(initialSinceDate).add(1, "day");
 
+
+
+const getLogs = async (discardHeaders, url) => {
+    try {
+        if (!discardHeaders) {
+            console.log('trigger')
+            const { data, records } = await getData(discardHeaders, url);
+            console.log('sum');
+            logs = logs + records
+            fs.writeFileSync(path, data, 'utf-8')
+        } else {
+            console.log('trigger');
+            const { data, records } = await getData(discardHeaders, url);
+            console.log('sum');
+            logs = logs + records
+            fs.appendFileSync(path, data, 'utf-8')
+        }
+        if (i >= diffDays) {
+            console.log(logs, ' records')
+            clearInterval(interval);
+        }
+
+    } catch (error) {
+        console.log(error)
+        clearInterval(interval)
+    }
+}
+
 const getLogsCSV = async () => {
 
-    let i = 0;
-    const interval = setInterval(() => {
+
+    interval = setInterval(() => {
+        console.log({ i })
         //making queries
 
         const sinceQuery = `${sinceDate.get("year")}-${Number(
@@ -36,14 +69,7 @@ const getLogsCSV = async () => {
             .padStart(2, 0)}-${sinceDate
                 .get("dates")
                 .toString()
-                .padStart(2, 0)}T${sinceDate
-                    .get("hours")
-                    .toString()
-                    .padStart(2, 0)}%3A${sinceDate
-                        .get("minutes")
-                        .toString()
-                        .padStart(2, 0)}%3A00`;
-        
+                .padStart(2, 0)}T06%3A00%3A00Z`;
         const untilQuery = `${untilDate.get("year")}-${Number(
             untilDate.get("months") + 1
         )
@@ -51,35 +77,17 @@ const getLogsCSV = async () => {
             .padStart(2, 0)}-${untilDate
                 .get("dates")
                 .toString()
-                .padStart(2, 0)}T${untilDate
-                    .get("hours")
-                    .toString()
-                    .padStart(2, 0)}%3A${untilDate
-                        .get("minutes")
-                        .toString()
-                        .padStart(2, 0)}%3A00`;
-
+                .padStart(2, 0)}T06%3A00%3A00Z`;
         const URLCompleted = `${url}since=${sinceQuery}&until=${untilQuery}&filter=eventType+eq+%22user.session.start%22+and+outcome.result+eq+%22FAILURE%22`;
         if (i <= firstcall) {
-            getData(false,URLCompleted)
-            .then((data) => {
-                fs.writeFileSync(path, data, 'utf-8')
-            })
-            .catch(error => { clearInterval(interval); console.log(error) });
+            getLogs(false, URLCompleted);
         } else {
-            getData(true,URLCompleted)
-            .then((data) => {
-                fs.appendFileSync(path, data, 'utf-8')
-            })
-            .catch(error => { clearInterval(interval); console.log(error) });
+            getLogs(true, URLCompleted);
         }
-        i++;
         untilDate.add(1, "day");
         sinceDate.add(1, "day");
-        if (i >= diffDays) {
-            clearInterval(interval);
-        }
-    }, 4000);
+        i++;
+    }, 10000);
 };
 
 getLogsCSV();
